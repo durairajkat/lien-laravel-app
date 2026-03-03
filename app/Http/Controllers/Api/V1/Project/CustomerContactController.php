5cd41d84fd42eec3bfc9e9c601320740fca32783
Durairaj <Durairaj@katprotech.com>
2026-03-02 23:58:58 -0800

    fetaure api delete added for customer contact

--- app/Http/Controllers/Api/V1/Project/CustomerContactController.php
+++ app/Http/Controllers/Api/V1/Project/CustomerContactController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Api\V1\Project;
 
 use App\Http\Controllers\Controller;
 use App\Http\Resources\CustomerContactResource;
+use App\Models\CompanyContact;
 use App\Models\MapCompanyContact;
 use Illuminate\Http\Request;
 
@@ -23,5 +24,28 @@ class CustomerContactController extends Controller
             ->paginate($perPage);
 
         return CustomerContactResource::collection($customerContacts);
+    }
+
+    public function deleteCustomerContact(Request $request)
+    {
+        $request->validate([
+            'id' => 'required|integer',
+        ]);
+
+        $id = $request->input('id');
+
+        $customerContact = MapCompanyContact::find($id);
+
+        if (!$customerContact) {
+            return response()->json(['message' => 'Customer contact not found.'], 404);
+        }
+
+        $contactId = $customerContact->company_contact_id;
+
+        CompanyContact::where('id', $contactId)->delete();
+
+        $customerContact->delete();
+
+        return response()->json(['message' => 'Customer contact deleted successfully.']);
     }
 }
