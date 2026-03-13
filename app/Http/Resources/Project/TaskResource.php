9690431255adf7bd85127e3558bcd7019ebbea70
Durairaj <Durairaj@katprotech.com>
2026-03-13 00:29:47 -0700

    task resource response is altered

--- app/Http/Resources/Project/TaskResource.php
+++ app/Http/Resources/Project/TaskResource.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Resources\Project;
 
+use Carbon\Carbon;
 use Illuminate\Http\Resources\Json\JsonResource;
 
 class TaskResource extends JsonResource
@@ -14,6 +15,22 @@ class TaskResource extends JsonResource
      */
     public function toArray($request)
     {
+        $priority = 'normal';
+
+        if ($this->complete_date) {
+            $priority = 'completed';
+        } elseif ($this->due_date) {
+
+            $dueDate = Carbon::parse($this->due_date);
+            $today = Carbon::today();
+
+            if ($dueDate->lt($today)) {
+                $priority = 'overdue';
+            } elseif ($dueDate->diffInDays($today) <= 3) {
+                $priority = 'near_due';
+            }
+        }
+        
         return [
             'id' => $this->id,
             'task_action_id' => $this->task_action_id,
@@ -25,8 +42,10 @@ class TaskResource extends JsonResource
             'assigned_to' => $this->assigned_to,
             'assigned_by' => $this->assigned_by,
             'assigned_at' => $this->assigned_at,
-            'status' => $this->status,
+            'status' => ucwords(str_replace('_', ' ', $this->status)),
             'actionType' => $this->actionType?->name,
+            'assigned_to_user' => $this->assignedToUser?->name,
+            'priority' => $priority
         ];
     }
 }
